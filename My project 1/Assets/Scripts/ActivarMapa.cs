19067e89cec10e30836d10d546dfafd8512bfcf3
Joakofrati <104913587+Joakofrati@users.noreply.github.com>
2024-04-27 13:45:13 -0300

    Mejoras en el MiniMapa y Mapa

--- My project 1/Assets/Scripts/ActivarMapa.cs
+++ My project 1/Assets/Scripts/ActivarMapa.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class ActivarMapa : MonoBehaviour
 {
-     private bool activeCanvasMiniMap;
+    private bool activeCanvasMiniMap;
     public GameObject CanvasMiniMap;
 
     void Start()
